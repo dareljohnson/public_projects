e3a57fe2d77c9bb6ce90675b14d1f5eef347e3d4
Darel Johnson <dareljohnson@gmail.com>
2012-07-24 23:07:25 -0400

    Finished Demo App.

--- demo_app/config/routes.rb
+++ demo_app/config/routes.rb
@@ -1,4 +1,9 @@
 DemoApp::Application.routes.draw do
+  # the link between resources and actions
+  resources :microposts
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
